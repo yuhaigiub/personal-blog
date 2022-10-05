59914369c0008970127720017d3e63848f53b20f
Yuhaigiub <buigiahuynt@gmail.com>
2022-10-05 15:56:16 +0700

    add Link to image and title

--- components/HomeComponent/MostPopular.js
+++ components/HomeComponent/MostPopular.js
@@ -7,10 +7,11 @@ const MostPopular = () => {
 			<h2 className="text-3xl font-semibold mb-9">
 				Most Popular
 			</h2>
-			<grid className="grid grid-rows-4 gap-y-4">
+			<grid className="grid grid-rows-4 gap-y-6">
 				<SmallArticle bg={"bg-red-300"} />
 				<SmallArticle bg={"bg-blue-300"} />
 				<SmallArticle bg={"bg-yellow-300"} />
+				<SmallArticle bg={"bg-cyan-300"} />
 				<SmallArticle bg={"bg-green-300"} />
 			</grid>
 		</div>
--- components/SmallArticle.js
+++ components/SmallArticle.js
@@ -1,14 +1,24 @@
 import React from "react";
+import Link from "next/link";
 
 const SmallArticle = ({ bg }) => {
 	return (
 		<div className="flex items-center gap-x-4">
-			<div className={`w-1/3 h-24 ${bg}`} />
+			{/*image here */}
+			<Link href="/">
+				<div
+					className={`w-1/3 h-24 ${bg} hover:cursor-pointer`}
+				/>
+			</Link>
 			<div>
 				<p className="text-slate-500">Tag</p>
-				<h3 className="text-2xl font-semibold">
-					This is a small Title
-				</h3>
+				<Link href="/">
+					<a className="hover:text-red-500">
+						<h3 className="text-2xl font-semibold">
+							This is a small Title
+						</h3>
+					</a>
+				</Link>
 				<div className="flex gap-x-4">
 					<p className="text-slate-500">4 Min Read</p>
 					<p className="text-slate-500">July 11, 2022</p>
